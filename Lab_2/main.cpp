73576bf286ed8542e1292d24f841bf680eef2a82
Yulia0627 <repetiy0905@gmail.com>
2025-10-19 13:34:04 +0300

    Added comparator

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -1,65 +1,48 @@
-#include "merge.h"
-#include <vector>
-using namespace std;
+// Compiler: Microsoft (R) C/C++ Optimizing Compiler Version 19.44.35216 for x64
+// Standart: C++20
 
-void print(Experiment obj) {
-    cout << "Sequential merge time without policy: " << obj.run_sequential_merge() << " ms\n";
-    cout << "Sequential merge time with policy: " << obj.merge_seq_policy() << " ms\n";
-    cout << "Parallel merge time: " << obj.merge_par_policy() << " ms\n";
-    cout << "Parallel unsequenced merge time: " << obj.merge_par_unseq_policy() << " ms\n";
-    vector<int> K_values = { 1, 2, 4, 8, 16 };
-    obj.test_parallel_merge(K_values);
+#include "merge.h"
+
+
+void print(Experiment& obj, const string& name, function<bool(int, int)> comp) {
+    cout << "\n=== Comparator: " << name << " ===\n";
+    cout << "Sequential merge time (no policy): "
+        << obj.run_sequential_merge(comp) << " ms\n";
+    cout << "Sequential merge time (seq policy): "
+        << obj.merge_seq_policy(comp) << " ms\n";
+    cout << "Parallel merge time (par policy): "
+        << obj.merge_par_policy(comp) << " ms\n";
+    cout << "Parallel unsequenced merge time: "
+        << obj.merge_par_unseq_policy(comp) << " ms\n";
+    obj.test_parallel_merge(comp);
 }
 
 int main() {
-    Experiment experiment_1(500);
-    print(experiment_1);
 
-    Experiment experiment_2(500, 2);
-    print(experiment_2);
+    function<bool(int, int)> comp_normal = [](int a, int b) { return a < b; };
 
-    Experiment experiment_3(500, 3);
-    print(experiment_3);
-    
-    Experiment experiment_4(50000);
-    print(experiment_4);
+    function<bool(int, int)> comp_slow = [](int a, int b) {
+        double x = std::sin(a) * std::cos(b) + std::sqrt(std::abs(a - b) + 1.0);
+        return x < 100.0;
+        };
 
-    Experiment experiment_5(50000, 2);
-    print(experiment_5);
+    Experiment experiment_1(100, comp_normal);
+    Experiment experiment_2(100, comp_slow);
+    Experiment experiment_3(10000, comp_normal);
+    Experiment experiment_4(10000, comp_slow);
+    Experiment experiment_5(3000000, comp_normal);
+    Experiment experiment_6(3000000, comp_slow);
 
-    Experiment experiment_6(50000, 3);
-    print(experiment_6);
+    print(experiment_1, "Normal comparator for 100 elements", comp_normal);
+    print(experiment_2, "Slow math comparator for 100 elements", comp_slow);
 
-    Experiment experiment_7(200000);
-    print(experiment_7);
+    print(experiment_3, "Normal comparator for 10000 elements", comp_normal);
+    print(experiment_4, "Slow math comparator for 10000 elements", comp_slow);
 
-    Experiment experiment_8(200000, 2);
-    print(experiment_8);
-    Experiment experiment_9(200000, 3);
-    print(experiment_9);
-
-    /*Experiment experiment_4(1000000);
-    print(experiment_4);*/
-
-    Experiment experiment_10(300000000);
-    print(experiment_10);
-
-    Experiment experiment_11(300000000, 2);
-    print(experiment_11);
-    Experiment experiment_12(300000000, 3);
-    print(experiment_12);
-    
-
-    /*Experiment exp(n);
-    exp.run_parallel_merge(2);
-
-    cout << "seq_1: ";
-    for (int v : exp.seq_1) cout << v << " ";
-    cout << "\nseq_2: ";
-    for (int v : exp.seq_2) cout << v << " ";
-    cout << "\nmerged: ";
-    for (int v : exp.merge_result) cout << v << " ";
-    cout << "\n";*/
+    print(experiment_5, "Normal comparator for 3000000 elements", comp_normal);
+    print(experiment_6, "Slow math comparator for 3000000 elements", comp_slow);
 
     return 0;
 }
+
+
